c27162f462e853894ead425616ef965c5718ffa4
Anil Antony <anilvithayathil007@gmail.com>
2024-12-14 21:33:29 +0530

    Notification complete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,6 +18,18 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
+              // Request permission to send notifications before launching the app
+            PermissionStatus status = await Permission.notification.request();
+
+            if (status.isGranted) {
+              // Show notification and launch the external app
+              await _showNotification();
+              launchAnotherApp();
+            } else {
+              // Handle case where permission is not granted
+              print("Permission not granted!");
+            }
+            //  requestNotificationPermissions();
             // Show notification
             await _showNotification();
             launchAnotherApp();
@@ -27,6 +40,18 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  
+// Future<void> requestNotificationPermissions() async {
+//    final PermissionStatus status = await Permission.notification.request();
+//    if (status.isGranted) {
+//       // Notification permissions granted
+//    } else if (status.isDenied) {
+//       // Notification permissions denied
+//    } else if (status.isPermanentlyDenied) {
+//       // Notification permissions permanently denied, open app settings
+//       await openAppSettings();
+//    }
+// }
   void launchAnotherApp() async {
     if (!await launchUrl(Uri.parse("https://www.youtube.com"),
         mode: LaunchMode.externalApplication)) {
@@ -56,6 +81,10 @@ class HomePage extends StatelessWidget {
       channelDescription: 'Notification from the current app',
       importance: Importance.high,
       priority: Priority.high,
+      ticker: 'ticker',
+      playSound: true, // Ensure sound plays with the notification
+      enableVibration: true, // Vibrate with the notification
+      visibility: NotificationVisibility.public, // Make it public
     );
 
     const iosDetails = DarwinNotificationDetails();
